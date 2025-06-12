1cdf39ae8f84917c4138951bf72793d26d13187f
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-12 15:12:23 -0300

    :books:  docs: tipos de dados em Python

---
+++ tipos_dados.py
@@ -0,0 +1,14 @@
+print(11 + 10 + 1000) # soma de inteiros
+print(1.5 + 1 + 0.5) # soma de floats
+print(True) # valor booleano verdadeiro
+print(False) # valor booleano falso
+print('Python') # string
+
+int() #converte para inteiro
+float() #converte para float
+bool() #converte para booleano
+str() #converte para string
+
+#Terminal: colocando Python e dar enter, ele entra em modo interativo
+# classes tem métodos e atributos
+# int, float, bool, str são classes
