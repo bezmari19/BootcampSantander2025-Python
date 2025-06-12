41c57a2bfb99cc6d6080d7544a245b3ad215d673
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-12 17:58:42 -0300

    :books: docs: convertendo variáveis

---
+++ convertendo_tipos.py
@@ -0,0 +1,15 @@
+print(int(1.97)) # Convertendo float para inteiro
+print(float(10)) # Convertendo inteiro para float
+print(int('10')) # Convertendo string para inteiro
+print(float('10.5')) # Convertendo string para float
+print(str(10)) # Convertendo inteiro para string
+
+#exemplos de conversão com base nos exemplos do curso
+
+valor = 10
+valor_string = str(valor) # Convertendo inteiro para string
+print(type(valor))
+print(type(valor_string)) # Verificando o tipo da variável
+
+print(100 / 2) # Divisão normal
+print(100 // 2) # Divisão inteira
