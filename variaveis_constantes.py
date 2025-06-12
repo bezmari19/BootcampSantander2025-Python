d7f98fcef09679b566b0b93e4a1aee1111ab637a
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-12 17:42:49 -0300

    :bulb: :books: variáveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,14 @@
+nome = 'Marina'
+idade = 27
+#exemplo separado
+
+nome, idade = 'Marina Bezerra', 27
+#exemplo junto e com menos linhas de código
+
+print(nome, idade)
+
+#exemplos
+limite_saque_direto = 1000 # Nome da variável em minúsculas, separada por underline e de fácil entendimento e leitura
+
+
+ESTADOS_BRASILEIROS = ['SP', 'RJ', 'MG', 'RS', 'SC', 'PR', 'BA', 'PE', 'CE', 'DF'] # Variável constante, por convenção, é escrita em letras maiúsculas - Snake Case
