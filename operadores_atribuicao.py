d6e0753d1304266ae7c4b82f03f1a59fabf045db
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-16 17:13:17 -0300

    :books: docs: exemplos aula operadores atribuição

---
+++ operadores_atribuicao.py
@@ -0,0 +1,37 @@
+#atribuição simples
+saldo = 100
+
+#atribuição com soma
+saldo = 100
+saldo += 200
+print(saldo) #300
+
+#atribuição com subtração
+saldo = 100
+saldo -= 30
+print(saldo) #70
+
+#atribuição com multiplicação
+saldo = 100
+saldo *= 2
+print(saldo) #200
+
+#atribuição com divisão
+saldo = 100
+saldo /= 2
+print(saldo) #50.0
+
+#atribuição com divisão inteira
+saldo = 100
+saldo //= 2
+print(saldo) #50
+
+#atribuição com módulo
+saldo = 100
+saldo %= 30
+print(saldo) #10
+
+#atribuição com exponenciação
+saldo = 100
+saldo **= 2
+print(saldo) #10000
