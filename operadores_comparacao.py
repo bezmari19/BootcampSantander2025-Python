fc12a06c6c8e3988acb14adf025bf3863d45d09f
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-16 16:59:17 -0300

    :books: docs: exemplos aula operadores

---
+++ operadores_comparacao.py
@@ -0,0 +1,11 @@
+saldo = 200
+saque = 200
+
+print(saldo == saque ) #verifica se o saldo é igual ao saque
+print(saldo != saque ) #verifica se o saldo é diferente do saque
+print(saldo > saque )  #verifica se o saldo é maior que o saque
+print(saldo < saque )  #verifica se o saldo é menor que o saque
+print(saldo >= saque ) #verifica se o saldo é maior ou igual ao saque
+print(saldo <= saque ) #verifica se o saldo é menor ou igual ao saque
+
+#Operadores de comparação retornam um valor booleano (true ou false)
