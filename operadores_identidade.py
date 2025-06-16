0610c70b023d953928567a5a599bf82a9dcfa464
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-16 18:06:35 -0300

    :bug: fix: escrita código

--- operadores_identidade.py
+++ operadores_identidade.py
@@ -2,7 +2,7 @@ saldo = 1000
 limite = 500
 
 #saber se ocupam a mesma região de memória
-print(saldo is limites) #False
+print(saldo is limite) #False
 print(saldo is saldo) #True
 print(saldo is not limite ) #True
 print(saldo is not saldo) #False
