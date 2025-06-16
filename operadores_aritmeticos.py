8faa27f178051c6587f92a3664fdcb58fcfb7801
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-16 16:50:33 -0300

    :books: docs: exemplos aula de operadores

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,24 @@
+produto_1 = 10
+produto_2 =  20
+
+#Soma
+print(produto_1 + produto_2)
+#subtração
+print(produto_1 - produto_2)
+#Multiplicação
+print(produto_1 * produto_2)
+#divisão
+print(produto_1 / produto_2)
+#Divisão inteira
+print(produto_1 // produto_2)
+#módulo
+print(produto_1 % produto_2)
+#Exponenciação
+print(produto_1 ** produto_2)
+
+#Contas mais complexas
+x = 10 + 5 * 4
+print(x)
+#ou 
+x = 10 + (5 * 4) #força a multiplicação ser feita primeiro
+print(x) #resposta diferente do exemplo anterior 
