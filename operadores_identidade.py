09fd0ab27b26982bd7bf6e95d6c7e91f3283677d
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-16 18:05:07 -0300

    :books: docs: exemplos aula operadores de identidade

---
+++ operadores_identidade.py
@@ -0,0 +1,9 @@
+saldo = 1000
+limite = 500
+
+#saber se ocupam a mesma região de memória
+print(saldo is limites) #False
+print(saldo is saldo) #True
+print(saldo is not limite ) #True
+print(saldo is not saldo) #False
+
