e3672d4f97b1b2dedddd995e2b85d6d3f0bd7ecb
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-25 17:11:31 -0300

    :books: docs: mais exemplos da aula de string de múltiplas linhas

--- string_multlinhas.py
+++ string_multlinhas.py
@@ -5,5 +5,10 @@ mensagem = f'''
 Olá, meu nome é {nome}!
 Eu estou aprendendo Python.
 '''
-# f''' texto '''
+# f''' texto + concatenação '''
 print(mensagem) #printa a mensagem que foi colocada
+
+#podendo ser feito da seguinte forma:
+print(f'''Olá, me chamo {nome} e eu estudo Python!\n''')
+print(f''' Bem vindo, ao meu github, onde posto todos os meu estudos 
+                    ********Menu******** ''')
