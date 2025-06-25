b958baee42a945a074968a1a0ee02ebfc686fa57
bezmari19 <marina.bezerraf19@gmail.com>
2025-06-25 16:58:09 -0300

    :books: docs: commit init primeiro exemplo

---
+++ string_multlinhas.py
@@ -0,0 +1,9 @@
+nome = 'Marina' #variável que aparecerá na mensagem
+#variável para a string de multiplas linhas:
+
+mensagem = f''' 
+Olá, meu nome é {nome}!
+Eu estou aprendendo Python.
+'''
+# f''' texto '''
+print(mensagem) #printa a mensagem que foi colocada
