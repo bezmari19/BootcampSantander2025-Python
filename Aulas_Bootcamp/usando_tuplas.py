58953fd723a8dc5243ce418b3cc5bdadf56b186e
bezmari19 <marina.bezerraf19@gmail.com>
2025-07-16 14:29:32 -0300

    :books: docs: aula de tuplas

---
+++ Aulas_Bootcamp/usando_tuplas.py
@@ -0,0 +1,13 @@
+#exemplos de tuplas
+frutas = ("laranja", 'pera', 'uva',)
+
+#é uma boa prática colocar virgula ao final
+letras = tuple('python')
+numero = tuple([1, 2, 3, 4])
+pais = ('Brasil',)
+
+#como usar as tuplas
+frutas[0] #laranja
+frutas[-1] #pera
+
+#tuplas não podem ser atualizadas
