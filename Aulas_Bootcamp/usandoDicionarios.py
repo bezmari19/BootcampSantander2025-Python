70c51b7c689811f27cac3da72ceff2771de0d7aa
bezmari19 <marina.bezerraf19@gmail.com>
2025-08-05 15:15:24 -0300

    :books: docs: aula sobre como usar dicionários em Python e seus métodos

--- Aulas_Bootcamp/usandoDicionarios.py
+++ Aulas_Bootcamp/usandoDicionarios.py
@@ -25,5 +25,34 @@ contatos = {"marina@gmail.com":{"nome":"Marina", "idade":27, "telefone":"3333--1
             }
 print(contatos["giovana@gmail.com"]["telefone"])
 
+#iterar dicionarios
+for chave in contatos:
+    print(chave, contatos[chave])
+#for chave, valor in contatos.items():
+    #print(chave, valor)
+#dicionarios não permitem que você repita chave
+
+
+#metodos da classe dict:
+#{}.clear = apaga todos os itens do dicionario
+#{}.copy = ele tira uma cópia do dicionário, virando OUTRO dicionário
+#o uso serve para quando não quer alterar o dicionario original
+#{}.fromkeys = 1 - quando quer criar chaves no dicionario sem colocar valor. 2 quando quer criar chaves e colocar valores padrão pode ou não ser existente
+#{}.get = uma segunda forma de acessar informações no dicionario caos não tenha certeza de há realmente aquela informação pela chave
+#{}.items
+#{}.keys = retorna só as chaves no dicionário
+#{}.pop = remover chaves e retorna o valor que ele encontrou para remoção
+#{}.popitem
+#{}.setdefault = se o atributo não estiver ele adiciona o valor que foi colocado ou se existir ele retorna o valor existente e não altera ele
+#{}.update = ele deixa atualizar o dicionário com outro dicionário
+#{}.values = ele retorna apenas o valores que tem nas chaves do dicionário
+# in = funciona também dentrod e uma lista/ verifica se há informações nas chaves do dicionario
+# del = tira um valor do dicionário que você passar para ele, exemplo passar o valor que deve ser retirado
+
+
+
+
+
+
 
 
